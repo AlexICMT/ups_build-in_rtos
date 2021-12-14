90552bac8e70212c241d014abd7c121c48b4ef10
AlexICMT <alexicmt1212@gmail.com>
2021-12-14 13:10:40 +0300

    init of project

---
+++ inc/FreeRTOSConfig.h
@@ -0,0 +1,111 @@
+/**************************************************************************//**
+ * @file FreeRTOSConfig.h
+ * @date 25.09.2015
+ * @author vlunyov
+ * @brief Настройки FreeRTOS
+ *
+ * Last modified by: $Author: vlunyov $
+ * $Revision: 1575 $
+ * $Date: 2017-03-07 09:49:41 +0300 (Вт, 07 мар 2017) $
+ *
+ * @note
+ * <h2><center>&copy; Copyright KW-Systems</center></h2>
+ * <h2><center><a href="https://kwsystems.ru/">KW-Systems</a></center></h2>
+ *
+ *****************************************************************************/
+
+#ifndef FREERTOS_CONFIG_H
+#define FREERTOS_CONFIG_H
+
+#define configUSE_PREEMPTION			1
+#define configUSE_IDLE_HOOK				0
+#define configUSE_TICK_HOOK				1
+#define configUSE_MALLOC_FAILED_HOOK	1
+#define configCPU_CLOCK_HZ				( ( uint32_t ) 80000000 )
+#define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
+#define configMAX_PRIORITIES			( 4 )
+#define configMINIMAL_STACK_SIZE		( ( uint16_t ) 50 )
+//#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 24 * 1024 ) )
+#define configMAX_TASK_NAME_LEN			( 8 )
+#define configUSE_16_BIT_TICKS			0
+#define configIDLE_SHOULD_YIELD			1
+#define configUSE_MUTEXES				1
+#define configUSE_RECURSIVE_MUTEXES		0
+#define configUSE_COUNTING_SEMAPHORES	1
+#define configCHECK_FOR_STACK_OVERFLOW	2
+#define configGENERATE_RUN_TIME_STATS	0
+
+#define configENABLE_BACKWARD_COMPATIBILITY		1
+
+#if defined(DEBUG)
+#define configUSE_TRACE_FACILITY				1
+#define configUSE_STATS_FORMATTING_FUNCTIONS	1
+#else
+#define configUSE_TRACE_FACILITY				1
+#define configUSE_STATS_FORMATTING_FUNCTIONS	1
+#endif
+
+// Co-routine definitions.
+#define configUSE_CO_ROUTINES 			0
+#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+
+// Software timer definitions.
+#define configUSE_TIMERS				0
+#define configTIMER_TASK_PRIORITY		( 2 )
+#define configTIMER_QUEUE_LENGTH		10
+#define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE * 2 )
+
+// Set the following definitions to 1 to include the API function, or zero to exclude the API function.
+
+#define INCLUDE_vTaskPrioritySet		0
+#define INCLUDE_uxTaskPriorityGet		0
+#define INCLUDE_vTaskDelete				1
+#define INCLUDE_vTaskCleanUpResources	0
+#define INCLUDE_vTaskSuspend			1
+#define INCLUDE_vTaskDelayUntil			1
+#define INCLUDE_vTaskDelay				1
+
+// Cortex-M specific definitions.
+#ifdef __NVIC_PRIO_BITS
+	// __BVIC_PRIO_BITS will be specified when CMSIS is being used.
+	#define configPRIO_BITS       		__NVIC_PRIO_BITS
+#else
+	#define configPRIO_BITS       		3        // 8 priority levels
+#endif
+
+// Interrupt priority levels:
+// 0 Наивысший приоритет прерываний
+// 1
+// 2
+// 3 FreeRTOS API configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY
+// 4 FreeRTOS API
+// 5 FreeRTOS API
+// 6 FreeRTOS API
+// 7 FreeRTOS API configKERNEL_INTERRUPT_PRIORITY Низший приоритет прерываний
+
+// The lowest interrupt priority that can be used in a call to a "set priority" function.
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY			( (1 << configPRIO_BITS ) - 1 )
+
+// The highest interrupt priority that can be used by any interrupt service
+// routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
+// INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
+// PRIORITY THAN THIS! (higher priorities are lower numeric values.
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY	3
+
+// Interrupt priorities used by the kernel port layer itself.  These are generic
+// to all Cortex-M ports, and do not rely on any particular library functions.
+#define configKERNEL_INTERRUPT_PRIORITY 		( configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
+// !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
+// See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html.
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
+
+
+// Definitions that map the FreeRTOS port interrupt handlers to their CMSIS standard names.
+#define vPortSVCHandler SVC_Handler
+#define xPortPendSVHandler PendSV_Handler
+#define xPortSysTickHandler SysTick_Handler
+
+//#define configASSERT( ( x ) )	if( ( x ) == 0) vAssertCalled(__FILE__, __LINE__)
+//#define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for(;;); }
+
+#endif // FREERTOS_CONFIG_H
---
+++ shared-libs/system/README.md
@@ -0,0 +1,74 @@
+# emdr1986x-std-per-lib
+## Milandr MCU 1986x Standard Peripherals Library
+
+This isn't official library that compatible with GCC compiler.
+
+**Now next MCU support gcc compiler:**
+- MDR1901VC1T (not tested)
+- MDR1986VE1T
+- MDR1986VE3 (not tested)
+- MDR1986BE4 (not tested)
+- MDR1986VE9x
+
+[**Link to documentation**](https://github.com/eldarkg/emdr1986x-std-per-lib-doc)
+
+[**Link to a recomended GCC compiler version**](https://launchpad.net/gcc-arm-embedded)
+
+Tested version of GCC compiler 4.9, 5.0.
+
+### Usage (pseudocode)
+```
+INC = inc
+      std-per-lib/CMSIS/CM3/CoreSupport
+      std-per-lib/CMSIS/CM3/DeviceSupport/MDR32F9Qx/inc
+      std-per-lib/CMSIS/CM3/DeviceSupport/MDR32F9Qx/startup
+      std-per-lib/MDR32F9Qx_StdPeriph_Driver/inc
+      std-per-lib/MDR32F9Qx_StdPeriph_Driver/inc/USB_Library
+```
+```
+SRC = example.c
+      std-per-lib/CMSIS/CM3/DeviceSupport/MDR32F9Qx/startup/gcc/startup_MDR32F9Qx.S
+      std-per-lib/CMSIS/CM3/DeviceSupport/MDR32F9Qx/startup/system_MDR32F9Qx.c
+      std-per-lib/MDR32F9Qx_StdPeriph_Driver/src/MDR32F9Qx_port.c
+      # another peripheral modules
+```
+***Release***
+```
+CFLAGS = -mcpu=cortex-m3 -mthumb -O2 -ffunction-sections -fdata-sections -Wall -Wextra -Wshadow -Wredundant-decls -Wno-missing-field-initializers -pipe
+```
+For each `src` in `SRC` list call:
+```
+arm-none-eabi-gcc ${CFLAGS} -DUSE_MDR1986VE9x -D__STARTUP_CLEAR_BSS -D__START=main -I${INC} -c ${src} -o ${src}.o
+```
+Link (`OBJ` it is a list of all `${src}.o`):
+```
+arm-none-eabi-gcc ${CFLAGS} -T std-per-lib/CMSIS/CM3/DeviceSupport/MDR32F9Qx/startup/gcc/MDR32F9Qx.ld --specs=nosys.specs -Wl,--gc-sections -ffreestanding -nostartfiles -Wl,-Map=example.map ${OBJ} -o example.elf
+```
+Get a hex file:
+```
+arm-none-eabi-objcopy -O ihex example.elf example.hex
+```
+***Debug***
+
+Instead `cflag` `-O2` set `-O0 -ggdb3`.
+
+***Select MCU architecture***
+
+Instead `cflag` `-mcpu=cortex-m3` use:
+```
+-mcpu=cortex-m0     # MDR1986BE4
+-mcpu=cortex-m1     # MDR1986VE1T, MDR1986VE3
+-mcpu=cortex-m3     # MDR1986VE9x, MDR1901VC1T
+```
+Instead compiler definition `-DUSE_MDR1986VE9x` use:
+```
+-DUSE_MDR1901VC1T   # MDR1901VC1T
+-DUSE_MDR1986VE1T   # MDR1986VE1T
+-DUSE_MDR1986VE3    # MDR1986VE3
+-DUSE_MDR1986BE4    # MDR1986BE4
+-DUSE_MDR1986VE9x   # MDR1986VE9x
+```
+`INC`, `SRC` and `ld script` change in accordance selected MCU.
+
+***Comment for pseudocode***
+- instead `${VAR}` paste relevant `VAR` list
